fb91898cea96aea510102de0f9ef715f9cfd8e30
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-07-23 10:54:37 +0200

    Fetching from the appi

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,57 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import Sidebar from "./components/Sidebar"
 
 
 function App() {
-  const NASA_KEY =import.meta.env.VITE_NASA_API_KEY
+  const [data, setData]= useState(null)
   const [showModal , setShowModal] = useState(false)
   function handleToggleModal() {
     // set opposite of the state
     setShowModal(!showModal)
   }
 
+  useEffect(()=>{
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY 
+      const url ='https://api.nasa.gov/planetary/apod'+`?api_key=${NASA_KEY}`
+
+      const today = (new Date()).toDateString()
+      const localKey = `NASA-${today}`
+      if (localStorage.getItem(localKey)){
+        const apiData = JSON.parse(localStorage.getItem(localKey))
+        setData(apiData)
+        return
+      }
+      localStorage.clear()
+      try{
+        const res = await fetch(url)
+        const apiData = await res.json()
+        localStorage.setItem(localKey,JSON.stringify(apiData))
+        setData(apiData)
+      }catch (err){
+        console.log(err.message)
+      }
+      
+    }
+    fetchAPIData()
+  },[])
+
   return (
     <>
-      <Main/>
+      {data ? (<Main data={data}/>):
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      }
       {showModal && (
-        <Sidebar handleToggleModal={handleToggleModal}/>
+        <Sidebar data={data} handleToggleModal={handleToggleModal}/>
       )}
-      <Footer handleToggleModal={handleToggleModal}/>
+      {data && (
+        <Footer data={data} handleToggleModal={handleToggleModal}/>
+      )}
+      
     </>
   )
 }
